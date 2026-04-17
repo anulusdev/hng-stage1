e86480036ef5fdeca76dbc2cfcc5d36c01f13897
AnulusDev <khalidalhazan@gmail.com>
2026-04-17 22:55:48 +0100

    proper url design

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('profiles', views.ProfileListView.as_view(), name='profiles-list'),
+    path('profiles/', views.ProfileListView.as_view(), name='profile-list-slash'),
     path('profiles/<str:profile_id>', views.ProfileDetailView.as_view(), name='profiles-detail'),
+    path('profiles/<str:profile_id>/', views.ProfileDetailView.as_view(), name='profile-detail-slash'),
 ]
